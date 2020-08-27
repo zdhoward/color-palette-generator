ef0a3ccc63d308782c9cd0959e4f2bf42bad87af
Zach Howard <obdo32@gmail.com>
2020-08-26 21:19:18 -0700

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+def main():
+    print("Hello World")
+    ###### USE HSV?
+    ## https://python-colormath.readthedocs.io/en/latest/
+    ## https://www.youtube.com/watch?v=u5AnzLg1HxY
+    ## SELECT BASE COLOR
+    ## use color curve to add/remove saturation and brightness
+    #### remove some brightness and saturation for 3 more colors, shift hue slightly down
+    #### add some brightness and saturation for 3 more colors, shift hue slightly up
+    ## add dimension by then  copying what we have and removing saturation while adding brightness and shifting hue slightly up
+    ## add dimension by then  copying what we have and adding saturation while removing brightness and shifting hue slightly down
+
+    ## allow for slight randomization so you can regen colour pallete
+    ## allow tweaking of base hue
+
+
+if __name__ == "__main__":
+    main()
