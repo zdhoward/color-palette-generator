e5cc17a33739712ca36c82d8fe76b4578075289b
= <=>
2020-08-30 16:29:57 -0700

    added subpalette prototype

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from random import randrange, shuffle
 
 app = Flask(__name__)
 
@@ -23,7 +24,7 @@ def html_form(hex, hue, sat, lum, is_sub=False, amount=0):
     table += f"<td><label for='Saturation Variation'>Saturation Variation:</label><input type='number' step='0.01' min='0.01' max='0.1' value={sat} id='saturation' name='saturation'></td>"
     table += f"<td><label for='Luminance Variation'>Luminance Variation:</label><input type='number' step='0.01' min='0.02' max='0.1' value={lum} id='luminance' name='luminance'></td>"
     if is_sub:
-        table += f"<td><label for='Number of Colours'>Number of Colours:</label><input type='number' min='0' max='6' value={amount} id='amount' name='amount'></td>"
+        table += f"<td><label for='Number of Palettes'>Number of Palettes:</label><input type='number' min='0' max='6' value={amount} id='amount' name='amount'></td>"
     table += "<td><input type='submit' value='Submit'></td>"
     table += "</form></tr>"
     table += "</table>"
@@ -45,16 +46,25 @@ def html_palette(hex_color, hue, saturation, luminance):
 
 
 def html_subpalettes(palette, amount):
+    page = ""
     subpalettes = generate_subpalettes(palette, amount)
-    return ""
+    page += "<table>"
+    id_counter = 1
+    for subpalette in subpalettes:
+        page += "<tr>"
+        for color in subpalette:
+            page += f"<td id={id_counter} class='hex-color' onClick='copycell({id_counter});'; align='center' height=33.33% width=14.25% style='background-color: {color};'>{color}</td>"
+        page += "</tr>"
+    page += "</table>"
+    return page
 
 
 def html_include_js():
-    return "<script src='static/color-generator.js'></script>"
+    return "<script src='/static/color-generator.js'></script>"
 
 
 def html_include_css():
-    return "<link rel='stylesheet' href='static/color-generator.css'>"
+    return "<link rel='stylesheet' href='/static/color-generator.css'>"
 
 
 @app.route("/")
@@ -116,11 +126,11 @@ def subpalette_index():
 
     page += "<body>"
 
-    page += html_palette(hex_color, hue, saturation, luminance)
+    # page += html_palette(hex_color, hue, saturation, luminance)
 
     ## generate subpalettes
     palette = generate_palette(hex_color, hue, saturation, luminance)
-    page = html_subpalettes(palette, amount)
+    page += html_subpalettes(palette, amount)
     page += html_form(hex_color, hue, saturation, luminance, True, amount)
     page += "</body>"
 
@@ -138,6 +148,15 @@ def rotate_hue(_hue, _diff):
 
 def generate_subpalettes(palette, amount):
     subpalettes = []
+    for i in range(amount):
+        subpalette = []
+        num_of_colors = 4  # randrange(3, 6, 1)
+        colors = list(range(7))
+        shuffle(colors)
+        for j in range(num_of_colors):
+            subpalette.append(palette[randrange(0, 3)][colors[j]])
+        subpalettes.append(subpalette)
+
     return subpalettes
 
 
